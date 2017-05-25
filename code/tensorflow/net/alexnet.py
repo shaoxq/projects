86e193cf4432dccf920adb0231cc73e4ad12984f
shaoxq <394434239@qq.com>
2017-05-25 11:34:18 +0800

    resnet script

--- code/tensorflow/net/alexnet.py
+++ code/tensorflow/net/alexnet.py
@@ -1,2 +1,37 @@
 # -*- coding: UTF-8 -*-
-  
+import tensorflow as tf
+from .network import Network
+
+class alexnet(Network):
+    def __init__(self, trainable=True):
+        self.inputs = []
+        self.data = tf.placeholder(tf.float32, shape=[None, None, None, 3])
+        self.label = tf.placeholder(tf.float32, shape=[None, 1])
+        self.keep_prob = tf.placeholder(tf.float32)
+        self.layers = dict({'data':self.data, 'label':self.label})
+        self.trainable = trainable
+        self.setup()
+
+    def setup(self):
+        (self.feed('data')
+             .conv(11, 11, 96, 4, 4, name='conv1')
+             .lrn(5, 0.0001, 0.75, name='norm1')
+             .max_pool(3, 3, 2, 2, name='pool1')
+             .conv(5, 5, 256, 1, 1, group=2, name='conv2')
+             .lrn(5, 0.0001, 0.75, name='norm2')
+             .max_pool(3, 3, 2, 2, name='pool2')
+             .conv(3, 3, 384, 1, 1, name='conv3')
+             .conv(3, 3, 384, 1, 1, group=2, name='conv4')
+             .conv(3, 3, 256, 1, 1, group=2, name='conv5')
+             .fc(4096, name='fc6')
+             .dropout(self.keep_prob, name='drop6')
+             .fc(4096, name='fc7')
+             .dropout(self.keep_prob, name='drop7')
+             .fc(1000, relu=False, name='fc8')
+             .softmax(name='prob'))
+
+    def build_loss():
+        predict = self.get_output('prob')
+        label = self.get_output('label')
+        return tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(logits=predict, labels=label))
+        
---
+++ object-detction/pvanet.md
@@ -0,0 +1,1 @@
+[Ä¿±ê¼ì²â--PVANET: Deep but Lightweight Neural Networks for Real-time Object Detection ](http://blog.csdn.net/cv_family_z/article/details/52438372)
